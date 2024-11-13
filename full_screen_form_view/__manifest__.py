107a14ed073452ec898e82c0f1f8941a04464e2c
Kevin Valentin <kevin.valentin@ife.de>
2024-11-13 13:03:51 +0100

    add full_screen_form_view

---
+++ full_screen_form_view/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': 'Full Screen Form View',
+    'version': '16.0.0.2',
+    'summary': 'Hide the chatter panel and make the form view is wider. New version makes the button is bigger and easy to click.',
+    'description': 'Hide the chatter panel and make the form view is wider. New version makes the button is bigger and easy to click.',
+    'author': "Sonny Huynh",
+    'category': 'Extra Tools',
+    'depends': ['base'],
+
+    'data': [],
+    'assets': {
+        'web.assets_backend': [
+            'full_screen_form_view/static/src/js/*.js',
+            'full_screen_form_view/static/src/scss/layout.scss',
+            'full_screen_form_view/static/src/xml/form_controller.xml',
+        ],
+    },
+
+    'demo': [],
+    'images': [
+        'static/description/banner.gif',
+    ],
+    'license': 'OPL-1',
+    'price': 39.00,
+    'currency': 'EUR',
+}
